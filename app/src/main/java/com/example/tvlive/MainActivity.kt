9359565637f6a7ba58578d34e8c53aca1b9a4335
fjgsxbn <chrdwh@outlook.com>
2025-07-31 12:33:26 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/tvlive/MainActivity.kt
+++ app/src/main/java/com/example/tvlive/MainActivity.kt
@@ -7,6 +7,7 @@ import android.preference.PreferenceManager
 import android.view.KeyEvent
 import android.view.View
 import android.widget.Button
+import android.widget.Switch
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
