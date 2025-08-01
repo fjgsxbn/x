314be539a3cdca64762b24410dc92c3ce8d6b2bc
fjgsxbn <chrdwh@outlook.com>
2025-08-01 08:34:49 +0800

    Update VideoPlayerManager.kt

--- app/src/main/java/com/example/tvlive/VideoPlayerManager.kt
+++ app/src/main/java/com/example/tvlive/VideoPlayerManager.kt
@@ -22,12 +22,12 @@ class VideoPlayerManager(context: Context) {
     data class Channel(val name: String, val url: String)
     private lateinit var channels: List<Channel>
 
-    fun p(addr1: String, callback: () -> Unit) {
+    fun p(adx: String, callback: () -> Unit) {
         context.lifecycleScope.launch {
             // 1. 启动协程（默认在主线程，但会被 withContext 切换）
             val client = OkHttpClient()
             val request = Request.Builder()
-                .url(addr1)
+                .url(adx)
                 .build()
             try {
                 // 发送同步请求（因在 IO 线程，不会阻塞主线程）
