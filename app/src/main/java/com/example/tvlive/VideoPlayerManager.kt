60192b90d2b18ab48c4cfe47fcbedc8e95971d23
fjgsxbn <chrdwh@outlook.com>
2025-08-01 08:29:33 +0800

    Update VideoPlayerManager.kt

--- app/src/main/java/com/example/tvlive/VideoPlayerManager.kt
+++ app/src/main/java/com/example/tvlive/VideoPlayerManager.kt
@@ -53,7 +53,7 @@ class VideoPlayerManager(context: Context) {
     }
 
 // 函数名改为小写 r，功能不变
-    suspend fun r(jsCode: String): List<*>? {
+    suspend fun r(jsCode: String){
         val context = Context.enter()
         return try {
             context.optimizationLevel = -1
@@ -64,7 +64,7 @@ class VideoPlayerManager(context: Context) {
             val jsonString = jsResult.toString()
 
             val gson = Gson()
-    channels = gson.fromJson(jsonStr, Array<MyData>::class.java).toList()
+    channels = gson.fromJson(jsonStr, Array<Channel>::class.java).toList()
         } catch (e: Exception) {
             e.printStackTrace()
             null
