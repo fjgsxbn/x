2b7652402eaedb6800052307609a470487c24b42
fjgsxbn <chrdwh@outlook.com>
2025-08-04 12:28:14 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/tvlive/MainActivity.kt
+++ app/src/main/java/com/example/tvlive/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.preference.PreferenceManager
 import android.view.KeyEvent
 import android.view.View
+import android.webkit.WebView
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Switch
@@ -34,7 +35,8 @@ class MainActivity : AppCompatActivity() {
 
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
         // 初始化播放器
-        playerManager = VideoPlayerManager(this, findViewById(R.id.webView))
+        val webView: WebView =findViewById(R.id.webView)
+        playerManager = VideoPlayerManager(this, webView)
         playerView = findViewById(R.id.player_view)
         playerView.player = playerManager.getPlayer()
         // playCurrentChannel()
