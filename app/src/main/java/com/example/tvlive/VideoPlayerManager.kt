21d2e153c5634f98729bea264ad0482e1b13ce8c
fjgsxbn <chrdwh@outlook.com>
2025-08-12 19:31:08 +0800

    Update VideoPlayerManager.kt

--- app/src/main/java/com/example/tvlive/VideoPlayerManager.kt
+++ app/src/main/java/com/example/tvlive/VideoPlayerManager.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.caoccao.javet.exceptions.JavetException
 import com.caoccao.javet.interop.V8Host
-import com.caoccao.javet.interop.runtime.V8Runtime
+import com.caoccao.javet.interop.V8Runtime
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.source.hls.HlsMediaSource
