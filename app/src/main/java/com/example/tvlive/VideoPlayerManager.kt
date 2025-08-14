c9ab691acc23d0def48cf96e44146a17995a3489
fjgsxbn <chrdwh@outlook.com>
2025-08-14 22:32:21 +0800

    Update VideoPlayerManager.kt

--- app/src/main/java/com/example/tvlive/VideoPlayerManager.kt
+++ app/src/main/java/com/example/tvlive/VideoPlayerManager.kt
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.caoccao.javet.interop.V8Host
 import com.caoccao.javet.interop.V8Runtime
+import com.caoccao.javet.runtime.V8Runtime
+import com.caoccao.javet.values.promise.IV8ValuePromise
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.source.hls.HlsMediaSource
@@ -101,9 +103,15 @@ class VideoPlayerManager(private val context: AppCompatActivity, private val web
             try {
                 // 初始化 Node.js 引擎（耗时操作，放后台）
                 v8Runtime = V8Host.getNodeInstance().createV8Runtime()
-                setupJsBridge()
-                v8Runtime?.getExecutor(jsCode)?.executeVoid()
-                // v8Runtime?.executeScript(jsCode)
+                //setupJsBridge()
+                //v8Runtime?.getExecutor(jsCode)?.executeVoid()
+                v8Runtime!!.getExecutor(jsCode)
+             .execute() // 执行 JS，返回 V8ValuePromise
+             ?.use { v8ValuePromise -> // Kotlin use 函数：自动关闭 V8ValuePromise（释放资源）
+                 v8ValuePromise.register(callback) // 注册回调
+                 v8Runtime.await() // 等待 Promise 完成（阻塞到回调触发）
+             }
+                
             } catch (e: Exception) {
                 Log.e("播放管理", "runjs", e)
             }
@@ -154,4 +162,22 @@ class VideoPlayerManager(private val context: AppCompatActivity, private val web
             channels = gson.fromJson(json, type)
         }
     }
+    private val callback = object : IV8ValuePromise.ICallback {
+         override fun onCatch(v8Value: V8Value) {
+             // 处理 Promise 内部未捕获的异常（如 JS 代码报错）
+             //assertTrue(v8Value is V8ValueError)
+             val error = v8Value as V8ValueError
+             println("Promise 捕获异常：${error.message}")
+             v8Value.close() // 释放 V8 资源（避免内存泄漏）
+         }
+         override fun onFulfilled(v8Value: V8Value) {
+             // 处理 Promise 成功（resolve 触发）
+             Log.i("收到promise",v8Value.toString())
+             v8Value.close() // 释放资源
+         }
+         override fun onRejected(v8Value: V8Value) {
+             // 处理 Promise 主动失败（reject 触发）
+             println("Promise 主动拒绝，原因：${v8Value.toString()}")
+             v8Value.close() // 释放资源
+         }
 }
