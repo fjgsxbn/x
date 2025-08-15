31f260a79e9d35ae9017ff7b2234396109a70fac
fjgsxbn <chrdwh@outlook.com>
2025-08-15 09:21:04 +0800

    Update VideoPlayerManager.kt

--- app/src/main/java/com/example/tvlive/VideoPlayerManager.kt
+++ app/src/main/java/com/example/tvlive/VideoPlayerManager.kt
@@ -108,7 +108,7 @@ class VideoPlayerManager(private val context: AppCompatActivity, private val web
                 // setupJsBridge()
                 // v8Runtime?.getExecutor(jsCode)?.executeVoid()
                 v8Runtime!!.getExecutor(jsCode)
-                    .execute()?.use { v8ValuePromise: IV8ValuePromise -> // Kotlin use 函数：自动关闭 V8ValuePromise（释放资源）
+                    .execute<IV8ValuePromise>()?.use { v8ValuePromise:  -> // Kotlin use 函数：自动关闭 V8ValuePromise（释放资源）
                         v8ValuePromise.register(callback) // 注册回调
                         v8Runtime!!.await() // 等待 Promise 完成（阻塞到回调触发）
                     }
