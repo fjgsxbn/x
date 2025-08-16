1871b463258aee3c7d5dda3c078aed69cc89f5d0
fjgsxbn <chrdwh@outlook.com>
2025-08-16 08:31:27 +0800

    Update VideoPlayerManager.kt

--- app/src/main/java/com/example/tvlive/VideoPlayerManager.kt
+++ app/src/main/java/com/example/tvlive/VideoPlayerManager.kt
@@ -109,8 +109,8 @@ class VideoPlayerManager(private val context: AppCompatActivity, private val web
                 v8Runtime = V8Host.getNodeInstance().createV8Runtime()
                 setupJsBridge()
                 // v8Runtime?.getExecutor(jsCode)?.executeVoid()
-                v8Runtime!!.getExecutor(jsCode)
-                    .executeVoid()
+                v8Runtime!!.getExecutor(jsCode).executeVoid()
+                v8Runtime!!.await()
             } catch (e: Exception) {
                 Log.e("播放管理", "runjs", e)
             }
