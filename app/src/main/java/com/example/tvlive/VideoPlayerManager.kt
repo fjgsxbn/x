ed2c8837e1d5bf866c53b68a97bc2d3c6ce599df
fjgsxbn <chrdwh@outlook.com>
2025-08-17 18:54:47 +0800

    Update VideoPlayerManager.kt

--- app/src/main/java/com/example/tvlive/VideoPlayerManager.kt
+++ app/src/main/java/com/example/tvlive/VideoPlayerManager.kt
@@ -20,7 +20,6 @@ import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.Dispatchers
-//import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
