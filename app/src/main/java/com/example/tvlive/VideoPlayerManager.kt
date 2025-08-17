b4cfe2c8c260c92791c8a27eba6414aeb9c24b3e
fjgsxbn <chrdwh@outlook.com>
2025-08-17 17:40:21 +0800

    Update VideoPlayerManager.kt

--- app/src/main/java/com/example/tvlive/VideoPlayerManager.kt
+++ app/src/main/java/com/example/tvlive/VideoPlayerManager.kt
@@ -126,10 +126,7 @@ class VideoPlayerManager(private val context: AppCompatActivity, private val web
     }
 
     private suspend fun setupJsBridge() {
-        // 2. 配置转换器，支持 Kotlin 与 JS 互操作
-        // val converter = JavetProxyConverter()
-        // v8Runtime?.setConverter(converter)
-        // 3. 创建 Kotlin 接收者实例，并注入到 Node.js 全局对象
+        
         val xtv = Xtv()
         val v8ValueObject: V8ValueObject = v8Runtime!!.createV8ValueObject()
         v8Runtime!!.globalObject!!.set("xtv", v8ValueObject)
