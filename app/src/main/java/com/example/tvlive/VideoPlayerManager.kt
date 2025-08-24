8d4a4084eb7b30f01b1368c43e506560b6d87c66
fjgsxbn <chrdwh@outlook.com>
2025-08-24 17:06:36 +0800

    Update VideoPlayerManager.kt

--- app/src/main/java/com/example/tvlive/VideoPlayerManager.kt
+++ app/src/main/java/com/example/tvlive/VideoPlayerManager.kt
@@ -25,7 +25,7 @@ import java.io.IOException
 import java.util.*
 
 class VideoPlayerManager(private val context: AppCompatActivity, private val webView: WebView) {
-    private lateinit var ijkPlayer: IjkMediaPlayer // IJKPlayer 核心实例
+    private  var ijkPlayer: IjkMediaPlayer =IjkMediaPlayer()
 
     fun getPlayer() = ijkPlayer
 
